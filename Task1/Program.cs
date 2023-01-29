e105991998684b1edf4390b8cd286942ccc6ac4e
Анечка-Бубочка <j.t.leroy@mail.ru>
2023-01-29 20:22:04 +0300

    added method PrintArray

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -14,3 +14,15 @@ int ProblemSolution(string[] array)
     }
     return count;
 }
+
+// Метод для вывода массива в консоль
+void PrintArray(string[] array)
+{
+    Console.Write(" -> [");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+
+        Console.Write(array[i] + " ");
+    }
+    Console.Write(array[array.Length - 1]+ "]");
+}
