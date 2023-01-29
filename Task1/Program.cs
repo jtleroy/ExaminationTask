4497624a459bea9064d885bd3f0d0dd388322d84
Анечка-Бубочка <j.t.leroy@mail.ru>
2023-01-29 20:20:43 +0300

    added method ProblemSolution

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+//метод, который находит количество элементов, отвечающих условию задачи
+int ProblemSolution(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+
+        if (array[i].Length <= 3)
+        {
+            count = count + 1;
+
+        }
+    }
+    return count;
+}
